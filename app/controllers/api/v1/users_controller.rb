97feaae48166803d901dad10c00c5fe763a85694
Matt Rangel <matt333three@aol.com>
2026-03-02 18:08:22 -0600

    Create users api controller with index and show methods

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :destroy]
+
+  def index
+    users = User.all
+    render json: users.to_json(only: [:id, :name])
+  end
+
+  def show
+    render json: @user.filtered
+  end
+
+  def show_current_user
+    render json: current_user.filtered(is_current_user: true)
+  end
+
+  def destroy
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
