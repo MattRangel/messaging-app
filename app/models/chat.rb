a8a2472f557fab5a0957541e100e8f2162bcc9d1
Matt Rangel <matt333three@aol.com>
2026-03-21 17:03:07 -0500

    Create of_users scope on Chat model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,11 @@
 class Chat < ApplicationRecord
   has_many :messages
   has_and_belongs_to_many :users
+
+  scope :of_users, ->(user_ids) {
+    joins(:users)
+      .group('chats.id')
+      .having("COUNT(users.id) = ?", user_ids.size)
+      .having("SUM(CASE WHEN users.id IN (?) THEN 1 ELSE 0 END) = ?", user_ids, user_ids.size)
+  }
 end
