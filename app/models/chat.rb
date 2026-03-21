0282c13f9b8dbd9766e100ace2ef5970fd222e33
Matt Rangel <matt333three@aol.com>
2026-03-21 17:05:12 -0500

    Implement find_or_create_of_users chat model method

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -8,4 +8,9 @@ class Chat < ApplicationRecord
       .having("COUNT(users.id) = ?", user_ids.size)
       .having("SUM(CASE WHEN users.id IN (?) THEN 1 ELSE 0 END) = ?", user_ids, user_ids.size)
   }
+
+  def self.find_or_create_of_users(user_ids)
+    self.of_users(user_ids).first ||
+      self.create(user_ids: user_ids)
+  end
 end
