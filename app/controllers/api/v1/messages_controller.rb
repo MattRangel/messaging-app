558710db38a09cae64fa2988164f88c7b1b4485e
Matt Rangel <matt333three@aol.com>
2026-03-23 17:04:48 -0500

    Implement create message on message API

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::MessagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    chat = params[:chat_id] ?
+      current_user.chats.find(params[:chat_id]) :
+      Chat.find_or_create_of_users([current_user.id, *params[:user_ids]])
+    message = Message.create(user: current_user, chat: chat, text: params[:text])
+    render json: message.as_json(only: [:created_at, :text], include: { user: { only: [:name, :id] } })
+  end
+end
