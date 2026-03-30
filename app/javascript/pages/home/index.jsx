285d233a71455cb97880d8c9e64418940412fbe1
Matt Rangel <matt333three@aol.com>
2026-03-30 18:50:07 -0500

    Add ChatList component to homepage

--- app/javascript/pages/home/index.jsx
+++ app/javascript/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import LoginWith from "@/components/auth/LoginWith";
+import ChatList from "@/components/ChatList";
 
 function Home() {
   return (
     <>
       <h1>Welcome to Chat App</h1>
       <LoginWith strategy="github" name="GitHub"/>
+      <ChatList />
     </>
   );
 }
