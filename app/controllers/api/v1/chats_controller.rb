39d957f1c54cbdb5cddcf97b36564c0c3060cf9a
Matt Rangel <matt333three@aol.com>
2026-04-02 18:04:45 -0500

    Include users and messages in API method chat#show response

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -10,7 +10,10 @@ class Api::V1::ChatsController < ApplicationController
   end
 
   def show
-    render json: @chats.find(params[:id])
+    render json: @chats.find(params[:id]).as_json(
+      only: [:id, :name],
+      include: { users: { only: [:name, :id] }, messages: { only: [:text, :user_id, :created_at] } }
+    )
   end
 
   private
