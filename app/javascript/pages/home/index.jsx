ca4969f5db62e59b7dcf7932eaf6c559ba26b345
Matt Rangel <matt333three@aol.com>
2026-04-02 18:11:47 -0500

    Add Chat component to Home

--- app/javascript/pages/home/index.jsx
+++ app/javascript/pages/home/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import LoginWith from "@/components/auth/LoginWith";
 import ChatList from "@/components/ChatList";
+import Chat from "@/components/Chat";
 
 function Home() {
   const [activeChatID, setActiveChatID] = useState(undefined);
@@ -10,6 +11,7 @@ function Home() {
       <h1>Welcome to Chat App</h1>
       <LoginWith strategy="github" name="GitHub"/>
       <ChatList activeChatID={activeChatID} setActiveChatID={setActiveChatID}/>
+      <Chat chatID={activeChatID}/>
     </>
   );
 }
