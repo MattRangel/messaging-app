1c40d7203b773e081ab1010c207c3180e3e22740
Matt Rangel <matt333three@aol.com>
2026-04-03 09:51:55 -0500

    Only inlcude user_id in message create API response

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -6,6 +6,6 @@ class Api::V1::MessagesController < ApplicationController
       current_user.chats.find(params[:chat_id]) :
       Chat.find_or_create_of_users([current_user.id, *params[:user_ids]])
     message = Message.create(user: current_user, chat: chat, text: params[:text])
-    render json: message.as_json(only: [:created_at, :text], include: { user: { only: [:name, :id] } })
+    render json: message.as_json(only: [:id, :text, :user_id, :created_at])
   end
 end
