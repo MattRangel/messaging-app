1bdcca504688d7619b26a9ffac11606f1e41a9cb
Matt Rangel <matt333three@aol.com>
2026-04-12 10:16:41 -0500

    Return error from API when an unauthorized request is made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  protected
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      render status: :unauthorized, json: { error: "Must be logged in to perform action" }
+    end
+  end
 end
