4a1a56ecfae320213fe3a3b97e354f7a6f961a87
Matt Rangel <matt333three@aol.com>
2026-05-03 18:27:06 -0500

    Return a fragment instead of div from Message component

--- app/javascript/components/Message.jsx
+++ app/javascript/components/Message.jsx
@@ -1,9 +1,11 @@
+import styles from "./Message.module.css";
+
 function Message({text, user, time}) {
   return (
-    <div>
+    <>
       <h4>From {user.name} @ {time}</h4>
       <p>{text}</p>
-    </div>
+    </>
   )
 }
 
