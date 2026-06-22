46c655727106e97e43833a37405a29dc764c7c6c
Matt Rangel <matt333three@aol.com>
2026-06-22 17:40:14 -0500

    Placeholder homepage

--- app/javascript/pages/home/index.jsx
+++ app/javascript/pages/home/index.jsx
@@ -1,17 +1,9 @@
-import { useState } from "react";
-import ChatList from "@/components/ChatList";
-import Chat from "@/components/Chat";
 import styles from "./style.module.css";
 
 function Home() {
-  const [activeChatID, setActiveChatID] = useState(undefined);
-
   return (
-    <div className={styles.area}>
-      <ChatList activeChatID={activeChatID} setActiveChatID={setActiveChatID}/>
-      <Chat chatID={activeChatID}/>
-    </div>
-  );
+    <h1>Welcome back!</h1>
+  )
 }
 
 export default Home;
