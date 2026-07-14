4286d379362a2956bfcc20e49b8e5c643643be60
Matt Rangel <matt333three@aol.com>
2026-07-14 18:57:10 -0500

    Implement mobile show / hide button

--- app/javascript/components/ChatList.jsx
+++ app/javascript/components/ChatList.jsx
@@ -1,40 +1,53 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link, useParams } from "react-router";
 import { getChatsList } from "@/helpers/messagingAPI";
 import styles from "./ChatList.module.css";
 
 function ChatList({ newChatOnClick }) {
   const [chats, setChats] = useState([]);
+  const [menuHidden, setMenuHidden] = useState(true);
   const { chatID: currentChatID } = useParams();
+
+  const menu = useRef();
+  const menuHiddenClass = menuHidden ? styles["portrait-hidden"] : "";
+  const toggleMenuHidden = () => {
+    setMenuHidden(menuHidden => !menuHidden);
+  }
 
   const updateChats = () => {
     getChatsList().then(setChats);
   }
   useEffect(() => updateChats(), []);
+  useEffect(() => setMenuHidden(true), [currentChatID]);
 
   return (
-    <ul className={styles["chat-list"]}>
-      <li>
+    <>
+      <button onClick={toggleMenuHidden} className={styles["toggle-menu"]}>
+        {menuHidden ? "⏷" : "⏶"}
+      </button>
+      <div ref={menu} className={`${styles.menu} ${menuHiddenClass}`}>
         <button
           className={styles["new-chat"]}
           onClick={newChatOnClick}
         >
           New Chat
         </button>
-      </li>
-      {chats.map((chat) => {
-        const isActive = chat.id == currentChatID;
-        return (
-          <li key={chat.id}>
-            <Link
-              to={`/chats/${chat.id}`}
-            >
-              {chat.name || "Unnamed Chat"}
-            </Link>
-          </li>
-        )
-      })}
-    </ul>
+        <ul className={styles["chat-list"]}>
+          {chats.map((chat) => {
+            const isActive = chat.id == currentChatID;
+            return (
+              <li key={chat.id}>
+                <Link className={isActive ? styles.active : ""}
+                  to={`/chats/${chat.id}`}
+                >
+                  {chat.name || "Unnamed Chat"}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </>
   );
 
 }
